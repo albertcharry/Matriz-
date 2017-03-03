c4ea5dfcf4169a48d202ab5ace4d5d4501e064d0
usuario <usuario@DESKTOP-R05DDNE>
2017-03-02 22:28:09 -0500

    se agrego

---
+++ encripptador/src/encripptador/Encripptador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encripptador;
+
+/**
+ *
+ * @author Albert Jose Charry, Jorge Andres Duran
+ * @version 1.1 02 de Marzo de 2017
+ */
+public class Encripptador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        interfac ob = new interfac();
+        ob.setVisible(true);
+        ob.setLocationRelativeTo(null);
+        
+    }
+    
+}
